cca4dba776a62152093cd9db010078a3ae0d723d
Pierre-Henri Gache <pierrehenri.gache@outlook.com>
2017-10-31 22:27:46 +0100

    init

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+ /// <reference types="vss-web-extension-sdk" />
+import * as tc from "telemetryclient-team-services-extension";
+import telemetryClientSettings = require("./telemetryClientSettings");
+class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement("span");
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+
+}
+
+const el = document.getElementById("content");
+const greeter = new Greeter(el);
+greeter.start();
+
+tc.TelemetryClient.getClient(telemetryClientSettings.settings).trackPageView("React Extension Hub.Index");
---
+++ src/telemetryClientSettings.ts
@@ -0,0 +1,9 @@
+import * as tc from "telemetryclient-team-services-extension";
+
+export const settings: tc.TelemetryClientSettings = {
+    key: "__InstrumentationKey__",
+    extensioncontext: "React Extension Hub",
+    disableTelemetry: "false",
+    disableAjaxTracking: "false",
+    enableDebug: "false"
+};
